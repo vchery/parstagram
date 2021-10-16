dc1397e09d525245f3b9793ed45f712a151d1130
vchery <valerie78@ufl.edu>
2021-10-16 12:26:06 -0400

    added part 2 functions

---
+++ Instagram/CommentCell.swift
@@ -0,0 +1,25 @@
+//
+//  CommentCell.swift
+//  Instagram
+//
+//  Created by Valerie Chery on 10/16/21.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var commentLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
